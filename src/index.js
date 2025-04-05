2d8f0a3c981ff71ed581d0c49964274647d38709
hochitrung8124 <hochitrung08012004@gmail.com>
2025-04-05 09:06:19 +0700

    5-4-25

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ const app = express()
 const port = 3000
 import morgan from 'morgan'
 import { engine } from 'express-handlebars';
+app.use(express.static('src/public'));
 //HTTP logger
 app.use(morgan('combined'))
 //template engine
-app.engine('handlebars', engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// chạy client
+// main has header and fooder
+app.engine(
+    ".hbs",
+    engine({
+      extname: ".hbs",
+     // partialsDir: "src/resources/view/partials", // ✅ Đăng ký thư mục chứa partials
+    })
+  );
+app.set('view engine', 'hbs');
 app.set('views', 'src/resources/view');
+// home has body
 app.get('/', (req, res) => {
     res.render('home');
 })
